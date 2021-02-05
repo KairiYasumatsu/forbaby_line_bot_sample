c3b598fb3f3705bd8c5ec272e0c8da04d91e355e
kairi Yasumatsu <kairi.matibuse@gmail.com>
2021-02-05 12:22:16 +0900

    修正

--- callback.php
+++ callback.php
@@ -61,7 +61,7 @@ if ($message->{"text"} == '確認') {
               array (
                 'type' => 'message',
                 'label' => '1ヶ月',
-                'text' => '妊娠月齢1カ月です',
+                'text' => '妊娠月齢2カ月です',
               ),
             ),
             1 => 
@@ -81,7 +81,7 @@ if ($message->{"text"} == '確認') {
               array (
                 'type' => 'message',
                 'label' => '3カ月',
-                'text' => '妊娠月齢3カ月です',
+                'text' => '妊娠月齢2カ月です',
               ),
             ),
             3 => 
@@ -91,7 +91,7 @@ if ($message->{"text"} == '確認') {
               array (
                 'type' => 'message',
                 'label' => '4カ月',
-                'text' => '妊娠月齢4カ月です',
+                'text' => '妊娠月齢2カ月です',
               ),
             ),
           ),
@@ -100,6 +100,90 @@ if ($message->{"text"} == '確認') {
 
      // カルーセルタイプ 
     // $messageData = [ 'type' => 'text', 'text' => "https://volare.slack.com/archives/DKJJ24Q22/p1611902930000600"."ユーザーIDのトークンをパラメタにつけてURLを作成→  ".$userid ];
+ } elseif ($message->{"text"} == '妊娠月齢2カ月です') {
+    $messageData = array (
+        'type' => 'text',
+        'text' => '目の特徴を以下からお選びください',
+        'quickReply' => 
+        array (
+          'items' => 
+          array (
+            0 => 
+            array (
+              'type' => 'action',
+              'action' => 
+              array (
+                'type' => 'message',
+                'label' => 'ぱっちり二重',
+                'text' => 'ぱっちり二重を選択',
+              ),
+            ),
+            1 => 
+            array (
+              'type' => 'action',
+              'action' => 
+              array (
+                'type' => 'message',
+                'label' => 'キレなが一重',
+                'text' => 'キレなが一重を選択',
+              ),
+            ),
+            2 => 
+            array (
+              'type' => 'action',
+              'action' => 
+              array (
+                'type' => 'message',
+                'label' => 'つぶら瞳',
+                'text' => 'つぶら瞳を選択',
+              ),
+            ),
+          ),
+        ),
+    );
+
+ } elseif ($message->{"text"} == 'ぱっちり二重を選択') {
+    $messageData = array (
+        'type' => 'text',
+        'text' => '鼻の特徴を以下からお選びください',
+        'quickReply' => 
+        array (
+          'items' => 
+          array (
+            0 => 
+            array (
+              'type' => 'action',
+              'action' => 
+              array (
+                'type' => 'message',
+                'label' => '小鼻が大きめの鼻',
+                'text' => '小鼻が大きめの鼻',
+              ),
+            ),
+            1 => 
+            array (
+              'type' => 'action',
+              'action' => 
+              array (
+                'type' => 'message',
+                'label' => '小鼻が小さめの鼻',
+                'text' => '小鼻が小さめの鼻',
+              ),
+            ),
+            2 => 
+            array (
+              'type' => 'action',
+              'action' => 
+              array (
+                'type' => 'message',
+                'label' => '低くて可愛い鼻',
+                'text' => '低くて可愛い鼻',
+              ),
+            ),
+          ),
+        ),
+    );
+
  } else {
      // それ以外は送られてきたテキストをオウム返し
      $messageData = [ 'type' => 'text', 'text' => $message->{"text"} ]; 
