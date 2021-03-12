a6a5427d24a5b0ba56a26a8aa24a20dbd165b314
kairi Yasumatsu <kairi.matibuse@gmail.com>
2021-03-12 12:21:56 +0900

    修正

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
   <meta property="og:image" content="https://jp.moony.com/content/dam/sites/jp_moony_com/images/tips_pregnancy/mv/pt0607.png">
   <meta property="og:title" content="妊娠初期の17の特徴とは？最短でいつからわかる？兆候と時期について-おむつのムーニー 公式 ユニ・チャーム">
   <meta property="og:description" content="妊娠初期に現れる症状は、いくつかあります。ちょっとした体調の変化で「もしかして妊娠？」と思ったけれど、妊娠検査薬で確認する時期にはまだ早いという人のために、妊娠初期の症状について産婦人科医の芥川修先生に話を聞きました。">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8K6X9HPPPR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8K6X9HPPPR');
+</script>
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
