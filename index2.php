6a31768d4f5dffcee67cd75a74d3c4739b8f92e0
kairi Yasumatsu <kairi.matibuse@gmail.com>
2021-03-12 12:08:50 +0900

    make it more better

---
+++ index2.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+  <meta property="og:title" content="【医師監修】妊娠初期（妊娠成立～妊娠15週）に知っておきたいこと">
+  <meta property="og:description" content="妊娠の兆候は？　妊娠が判明したら気をつけたいことや、心構えは？“つわり”ってどんなもの？　妊娠初期について詳しく見ていきましょう。">
+  <meta property="og:type" content="article">
+  <meta property="og:image" content="https://st.benesse.ne.jp/navi_img/ninshin/ninshinshoki_img1.jpg">
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
